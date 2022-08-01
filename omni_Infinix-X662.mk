db356831950cb8385b3a8555a6e3b7fd981a4986
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-01 21:54:40 +0200

    Infinix-X662: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: infinix Model: Infinix X662 Script version: 2.4.0

---
+++ omni_Infinix-X662.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from Infinix-X662 device
+$(call inherit-product, device/infinix/Infinix-X662/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Infinix-X662
+PRODUCT_NAME := omni_Infinix-X662
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X662
+PRODUCT_MANUFACTURER := infinix
